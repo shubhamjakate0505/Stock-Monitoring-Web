876eac71c8f61dbf3c088b5e5f91a74bc37e04d4
jenil-desai <jenildev91@gmail.com>
2024-07-31 22:57:09 +0530

    Version 2.2

--- frontend/src/landing_page/home/Hero.js
+++ frontend/src/landing_page/home/Hero.js
@@ -1,7 +1,18 @@
 import React from "react";
 
 function Hero() {
-  return <h1>Hero</h1>;
+  return (
+    <div className="container p-5 mb-5">
+      <div className="row text-center">
+        <img src="media/images/homeHero.png" alt="Hero Image" className="mb-5" />
+        <h1 className="mt-5">Invest in everything</h1>
+        <p>Online platform to invest in stocks, derivatives, mutual funds, and more</p>
+        <button style={{ width: "20%", margin: "0 auto" }} className="p-2 btn btn-primary fs-5">
+          Sing up now
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default Hero;
