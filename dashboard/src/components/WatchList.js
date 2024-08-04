3265c2f1f39c3e807f77dba211f35e35b447c699
jenil-desai <jenildev91@gmail.com>
2024-08-04 16:30:31 +0530

    Version 5.1

--- dashboard/src/components/WatchList.js
+++ dashboard/src/components/WatchList.js
@@ -1,20 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
+
+import { watchlist } from "../data/data";
+import WacthListItem from "./WatchListItem";
 
 const WatchList = () => {
   return (
     <div className="watchlist-container">
       <div className="search-container">
-        <input
-          type="text"
-          name="search"
-          id="search"
-          placeholder="Search eg:infy, bse, nifty fut weekly, gold mcx"
-          className="search"
-        />
-        <span className="counts"> 9 / 50</span>
+        <input type="text" name="search" id="search" placeholder="Search eg:infy, bse, nifty fut weekly, gold mcx" className="search" />
+        <span className="counts"> {watchlist.length} / 50</span>
       </div>
 
-      <ul className="list"></ul>
+      <ul className="list">
+        {watchlist.map((stock, index) => {
+          return <WacthListItem stock={stock} key={index} />;
+        })}
+      </ul>
     </div>
   );
 };
