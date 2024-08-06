a12e0b47c452ffa89890569d8fa6d92a73b260d0
jenil-desai <jenildev91@gmail.com>
2024-08-06 17:38:20 +0530

    Version 7.1.1

--- frontend/src/landing_page/Navbar.js
+++ frontend/src/landing_page/Navbar.js
@@ -15,7 +15,7 @@ function Navbar() {
           <form className="d-flex" role="search">
             <ul className="navbar-nav mb-lg-0">
               <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#">
+                <a className="nav-link active" aria-current="page" href="https://zerodha-clone-dashboard.vercel.app/register">
                   Register
                 </a>
               </li>
